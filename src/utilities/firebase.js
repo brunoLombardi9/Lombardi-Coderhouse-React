135a6424cb1970262e5f4d9179608d82129b56c0
Bruno <juegos.malafar@gmail.com>
2022-07-10 17:58:31 -0300

    Se colocan variables de entorno en .env
    se colcan keys de firebase dentro de variables de entorno

--- src/utilities/firebase.js
+++ src/utilities/firebase.js
@@ -1,13 +1,14 @@
 import { initializeApp } from "firebase/app";
 import {getFirestore} from 'firebase/firestore';
 
+
 const firebaseConfig = {
-  apiKey: "AIzaSyDml_knBsT_9_w2j_4pvPt9cf8rTb-9ynk",
-  authDomain: "react-coderhouse-8a758.firebaseapp.com",
-  projectId: "react-coderhouse-8a758",
-  storageBucket: "react-coderhouse-8a758.appspot.com",
-  messagingSenderId: "494072448266",
-  appId: "1:494072448266:web:05e936a6921986e49d26f3"
+  apiKey: process.env.REACT_APP_apiKey,
+  authDomain: process.env.REACT_APP_authDomain,
+  projectId: process.env.REACT_APP_projectId,
+  storageBucket: process.env.REACT_APP_storageBucket,
+  messagingSenderId: process.env.REACT_APP_messagingSenderId,
+  appId: process.env.REACT_APP_appId
 };
 
 export const app = initializeApp(firebaseConfig);
